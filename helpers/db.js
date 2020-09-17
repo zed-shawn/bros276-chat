ddec9bccfcf098b7b37a0689e3a4e042ad7c9224
zed.shawn <alizeeshan1999@gmail.com>
2020-09-18 01:07:03 +0530

    v1.1

--- helpers/db.js
+++ helpers/db.js
@@ -4,7 +4,7 @@ const db = SQLite.openDatabase("local.db");
 
 export const init = () => {
   const promise = new Promise((resolve, reject) => {
-    /* db.transaction((tx) => {
+    db.transaction((tx) => {
       tx.executeSql(
         "DROP TABLE chatStore",
         [],
@@ -15,7 +15,7 @@ export const init = () => {
           reject(err);
         }
       );
-    });  */
+    }); 
     db.transaction((tx) => {
       tx.executeSql(
         "CREATE TABLE IF NOT EXISTS chatStore (id INTEGER PRIMARY KEY NOT NULL, sender TEXT NOT NULL, content TEXT NOT NULL, timestamp TEXT NOT NULL, color TEXT);",
@@ -28,7 +28,7 @@ export const init = () => {
         }
       );
     });
-/*     db.transaction((tx) => {
+    db.transaction((tx) => {
       tx.executeSql(
         "DROP TABLE userDetail",
         [],
@@ -39,7 +39,7 @@ export const init = () => {
           reject(err);
         }
       );
-    }); */
+    });
     db.transaction((tx) => {
       tx.executeSql(
         "CREATE TABLE IF NOT EXISTS userDetail (hashID TEXT PRIMARY KEY NOT NULL, name TEXT NOT NULL);",
